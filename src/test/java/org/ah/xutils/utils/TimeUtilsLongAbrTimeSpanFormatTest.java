6364955e766176c7a01e00a6684b5a386947705d
Armend Hoxha <ahoxha4biz@gmail.com>
2019-10-31 10:54:29 +0100

    Convert TimeUtilsLongAbrTimeSpanFormatTest.java from junit4 to junit5.

--- src/test/java/org/ah/xutils/utils/TimeUtilsLongAbrTimeSpanFormatTest.java
+++ src/test/java/org/ah/xutils/utils/TimeUtilsLongAbrTimeSpanFormatTest.java
@@ -1,55 +1,51 @@
 package org.ah.xutils.utils;
 
-import static org.ah.xutils.def.InMilliseconds.days;
-import static org.ah.xutils.def.InMilliseconds.hours;
-import static org.ah.xutils.def.InMilliseconds.minutes;
-import static org.ah.xutils.def.InMilliseconds.seconds;
-import static org.junit.Assert.assertEquals;
-
 import org.ah.xutils.def.TimeSpan;
 import org.ah.xutils.def.formatters.TimeSpanLongAbbreviatedFormatter;
-import org.ah.xutils.utils.TimeUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class TimeUtilsLongAbrTimeSpanFormatTest {
-	@Test
-	public void testSeconds() {
-		long start = System.currentTimeMillis();
-		long end = start + seconds(5);
-		TimeSpan ts = TimeUtils.diff(start, end);
-		assertEquals("0 d, 0 h, 0 m, 5 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
-	}
+import static org.ah.xutils.def.InMilliseconds.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-	@Test
-	public void testMinutes() {
-		long start = System.currentTimeMillis();
-		long end = start + minutes(12);
-		TimeSpan ts = TimeUtils.diff(start, end);
-		assertEquals("0 d, 0 h, 12 m, 0 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
-	}
+class TimeUtilsLongAbrTimeSpanFormatTest {
+    @Test
+    void testSeconds() {
+        long start = System.currentTimeMillis();
+        long end = start + seconds(5);
+        TimeSpan ts = TimeUtils.diff(start, end);
+        assertEquals("0 d, 0 h, 0 m, 5 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
+    }
 
-	@Test
-	public void testHours() {
-		long start = System.currentTimeMillis();
-		long end = start + hours(10);
-		TimeSpan ts = TimeUtils.diff(start, end);
-		assertEquals("0 d, 10 h, 0 m, 0 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
-	}
+    @Test
+    void testMinutes() {
+        long start = System.currentTimeMillis();
+        long end = start + minutes(12);
+        TimeSpan ts = TimeUtils.diff(start, end);
+        assertEquals("0 d, 0 h, 12 m, 0 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
+    }
 
-	@Test
-	public void testDays() {
-		long start = System.currentTimeMillis();
-		long end = start + days(12);
-		TimeSpan ts = TimeUtils.diff(start, end);
-		assertEquals("12 d, 0 h, 0 m, 0 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
-	}
+    @Test
+    void testHours() {
+        long start = System.currentTimeMillis();
+        long end = start + hours(10);
+        TimeSpan ts = TimeUtils.diff(start, end);
+        assertEquals("0 d, 10 h, 0 m, 0 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
+    }
 
-	@Test
-	public void testDaysHoursMinutesSeconds() {
-		long start = System.currentTimeMillis();
-		long end = start + days(2) + hours(9) + minutes(4) + seconds(5);
-		TimeSpan ts = TimeUtils.diff(start, end);
-		assertEquals("2 d, 9 h, 4 m, 5 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
-	}
+    @Test
+    void testDays() {
+        long start = System.currentTimeMillis();
+        long end = start + days(12);
+        TimeSpan ts = TimeUtils.diff(start, end);
+        assertEquals("12 d, 0 h, 0 m, 0 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
+    }
+
+    @Test
+    void testDaysHoursMinutesSeconds() {
+        long start = System.currentTimeMillis();
+        long end = start + days(2) + hours(9) + minutes(4) + seconds(5);
+        TimeSpan ts = TimeUtils.diff(start, end);
+        assertEquals("2 d, 9 h, 4 m, 5 s", ts.format(new TimeSpanLongAbbreviatedFormatter()));
+    }
 
 }
