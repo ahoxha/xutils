cb5e954a198593fda3b03e42514e8b3f5178233b
Armend Hoxha <hoxhaarmend@gmail.com>
2020-02-24 21:32:55 +0100

    Refactor code.

--- src/main/java/org/ah/xutils/utils/Numbers.java
+++ src/main/java/org/ah/xutils/utils/Numbers.java
@@ -33,43 +33,43 @@ public final class Numbers {
      * @param delimiter A string that is put between words, usually a space, but it can be any string. Examples: 'twenty one', 'twenty-one', 'twenty_one'.
      */
     public static String toWords(int number, String delimiter) {
-        String numInWords = "N/A";
+        String result = "N/A";
         if (number >= 0 && number <= 20) {
-            numInWords = NumberAsWord.get(number);
+            result = NumberAsWord.get(number);
         } else if (number < 100) {
             int tens = (number / 10) * 10;
-            numInWords = NumberAsWord.get(tens);
+            result = NumberAsWord.get(tens);
             int ones = number - tens;
             if (ones > 0) {
-                numInWords += delimiter + NumberAsWord.get(ones);
+                result += delimiter + NumberAsWord.get(ones);
             }
         } else if (number < 1000) {
             int hundreds = number / 100;
-            numInWords = NumberAsWord.get(hundreds) + delimiter + NumberAsWord.get(100);
+            result = NumberAsWord.get(hundreds) + delimiter + NumberAsWord.get(100);
             int rest = number - hundreds * 100;
             if (rest < 20 && rest > 0) {
-                numInWords += delimiter + "and" + delimiter + NumberAsWord.get(rest);
+                result += delimiter + "and" + delimiter + NumberAsWord.get(rest);
             } else {
                 int tenth = ((number - hundreds * 100) / 10) * 10;
                 if (tenth > 0) {
-                    numInWords += delimiter + "and" + delimiter + NumberAsWord.get(tenth);
+                    result += delimiter + "and" + delimiter + NumberAsWord.get(tenth);
                 }
                 int basic = number - hundreds * 100 - tenth;
                 if (basic > 0) {
-                    numInWords += delimiter + NumberAsWord.get(basic);
+                    result += delimiter + NumberAsWord.get(basic);
                 }
             }
         } else if (number < 1_000_000) {
             int thousands = number / 1000;
-            numInWords = toWords(thousands, delimiter) + delimiter + NumberAsWord.get(1000) +
+            result = toWords(thousands, delimiter) + delimiter + NumberAsWord.get(1000) +
                     (number - thousands * 1000 > 0 ? delimiter + "and" + delimiter : "")
                     + toWords(number - thousands * 1000, delimiter);
         } else if (number < 1_000_000_000) {
             int millions = number / 1_000_000;
-            numInWords = toWords(millions, delimiter) + delimiter + NumberAsWord.get(1_000_000) +
+            result = toWords(millions, delimiter) + delimiter + NumberAsWord.get(1_000_000) +
                     (number - millions * 1_000_000 > 0 ? delimiter + "and" + delimiter : "")
                     + toWords(number - millions * 1_000_000, delimiter);
         }
-        return numInWords;
+        return result;
     }
 }
