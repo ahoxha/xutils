70393c1727d183774d8a32a0ea942157f60c8820
Hoxha <armend.hoxha@rabobank.nl>
2021-03-15 17:14:51 +0100

    Refactoring: Introducres ArgumentsUtil to make tests more readable.

--- src/test/java/org/ah/xutils/utils/StringsTest.java
+++ src/test/java/org/ah/xutils/utils/StringsTest.java
@@ -1,11 +1,14 @@
 package org.ah.xutils.utils;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.ah.xutils.utils.StringsTest.ArgumentsUtil.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.params.provider.Arguments.of;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Base64.Encoder;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -48,44 +51,108 @@ class StringsTest {
         assertEquals(expectedOutput, Strings.toCamelCase(input, prefix));
     }
 
-    private static Stream<Arguments> getInputArgumentsWithPrefix() {
-        return Stream.of( //
-                of("NAME_AND_SURNAME", "add", "addNameAndSurname"), //
-                of("name", "add", "addName"), //
-                of("FIRST_NAME", "add", "addFirstName"), //
-                of("NAME_AND_SURNAME_AND_MIDDLE_NAME", "add", "addNameAndSurnameAndMiddleName"),//
-                of("AB_cD_and_a", "add", "addAbCdAndA"), //
-                of("_ABC_DEF", "add", "addAbcDef"), //
-                of("ABC_DEF_", "add", "addAbcDef"), //
-                of("_ABC_DEF_", "add", "addAbcDef"), //
-                of("A_b_c_D", "add", "addABCD"), //
-                of("CELSIUS_TO_FAHRENHEIT_CONVERTER", "", "CelsiusToFahrenheitConverter"), //
-                of("a", "", "A"), //
-                of("tESt", "", "Test"), //
-                of(null, "", ""), //
-                of("", "", ""), //
-                of("", "add", ""), //
-                of(null, null, ""), //
-                of("test", null, "") //
-        );
-    }
-
     @ParameterizedTest(name = "''{0}'' must be converted into ''{1}''")
     @MethodSource("getInputArgumentsWithoutPrefix")
     void testToCamelCaseWithoutPrefix(String input, String expectedOutput) {
         assertEquals(expectedOutput, Strings.toCamelCase(input));
     }
 
+    private static Stream<Arguments> getInputArgumentsWithPrefix() {
+        return Stream.of( //
+                given("NAME_AND_SURNAME") //
+                        .withPrefix("add") //
+                        .thenExpect("addNameAndSurname"), //
+                given("name") //
+                        .withPrefix("add") //
+                        .thenExpect("addName"), //
+                given("FIRST_NAME") //
+                        .withPrefix("add") //
+                        .thenExpect("addFirstName"), //
+                given("NAME_AND_SURNAME_AND_MIDDLE_NAME") //
+                        .withPrefix("add") //
+                        .thenExpect("addNameAndSurnameAndMiddleName"),//
+                given("AB_cD_and_a") //
+                        .withPrefix("add") //
+                        .thenExpect("addAbCdAndA"), //
+                given("_ABC_DEF") //
+                        .withPrefix("add") //
+                        .thenExpect("addAbcDef"), //
+                given("ABC_DEF_") //
+                        .withPrefix("add") //
+                        .thenExpect("addAbcDef"), //
+                given("_ABC_DEF_") //
+                        .withPrefix("add") //
+                        .thenExpect("addAbcDef"), //
+                given("A_b_c_D") //
+                        .withPrefix("add") //
+                        .thenExpect("addABCD"), //
+                given("CELSIUS_TO_FAHRENHEIT_CONVERTER") //
+                        .withPrefix("") //
+                        .thenExpect("CelsiusToFahrenheitConverter"), //
+                given("a") //
+                        .withPrefix("") //
+                        .thenExpect("A"), //
+                given("tESt") //
+                        .withPrefix("") //
+                        .thenExpect("Test"), //
+                given((Object) null) //
+                        .withPrefix("") //
+                        .thenExpect(""), //
+                given("") //
+                        .withPrefix("") //
+                        .thenExpect(""), //
+                given("") //
+                        .withPrefix("add") //
+                        .thenExpect(""), //
+                given((Object) null) //
+                        .withPrefix(null) //
+                        .thenExpect(""), //
+                given("test") //
+                        .withPrefix(null) //
+                        .thenExpect("") //
+        );
+    }
+
     private static Stream<Arguments> getInputArgumentsWithoutPrefix() {
         return Stream.of( //
-                of("COSMIC_MICROWAVE_BACKGROUND_RADIATION", "CosmicMicrowaveBackgroundRadiation"), //
-                of("first name", "FirstName"), //
-                of("last-name", "LastName"), //
-                of("my custom -class", "MyCustomClass"), //
-                of("my:custom:class", "MyCustomClass"), //
-                of("My;custoM;class", "MyCustomClass"), //
-                of("My;custoM;class;", "MyCustomClass"), //
-                of("my=custom=class", "MyCustomClass") //
+                given("COSMIC_MICROWAVE_BACKGROUND_RADIATION") //
+                        .thenExpect("CosmicMicrowaveBackgroundRadiation"), //
+                given("first name") //
+                        .thenExpect("FirstName"), //
+                given("last-name") //
+                        .thenExpect("LastName"), //
+                given("my custom -class") //
+                        .thenExpect("MyCustomClass"), //
+                given("my:custom:class") //
+                        .thenExpect("MyCustomClass"), //
+                given("My;custoM;class") //
+                        .thenExpect("MyCustomClass"), //
+                given("My;custoM;class;") //
+                        .thenExpect("MyCustomClass"), //
+                given("my=custom=class") //
+                        .thenExpect("MyCustomClass") //
         );
+    }
+
+    public static class ArgumentsUtil {
+        private final List<Object> objects;
+
+        private ArgumentsUtil(List<Object> ob) {
+            this.objects = ob;
+        }
+
+        public static ArgumentsUtil given(Object... args) {
+            return new ArgumentsUtil(new ArrayList<>(Arrays.asList(args)));
+        }
+
+        public ArgumentsUtil withPrefix(Object obj) {
+            objects.add(obj);
+            return this;
+        }
+
+        public Arguments thenExpect(Object result) {
+            objects.add(result);
+            return Arguments.of(objects.toArray());
+        }
     }
 }
